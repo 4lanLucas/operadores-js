bffa4c3c02c0211c92d1d4887a4711c011f13662
4lanLucas <alanlucas0707@gmail.com>
2021-12-31 08:15:17 -0300

    add chek se é maior ou menor que 20

---
+++ README.md
@@ -0,0 +1,9 @@
+#Projeto de sintaxe e operadores 
+
+Script em js feito com o bootcamp da inter pela DIO.
+funcionalidades: 
+
+- recebe dois parâmetros; ✅
+- diz se são iguais ou diferentes;✅
+- confere se sua soma é maior que 10 ou menor que 20;✅
+- retorna uma string com o resultado;✅
--- script2.js
+++ script2.js
@@ -1,14 +1,33 @@
 function recebe(n1,n2){
     num1 = n1;
     num2 = n2;
-    return result1();
+    return result1() + result2();
 }
 function result1(){
      if (num1 !== num2) {
-        return ("os números "+ num1 + " e "+ num2 +" são diferentes");
+        return ("os números "+ num1 + " e "+ num2 +" são diferentes ");
      } else {
-        return ("os números "+ num1 + " e "+ num2 +" são iguais");
+        return ("os números "+ num1 + " e "+ num2 +" são iguais ");
      }
 }
-
-console.log(recebe(1,2));
+function result2(){
+   let rs1; 
+   let rs2;
+   var soma = (num1 + num2);
+    if (soma > 10){
+      rs1 = "maior";
+   } else if (soma < 10){
+      rs1 = "menor";
+   } else {
+      rs1 = "igual";
+   } 
+   if (soma > 20){
+      rs2 = "maior";
+   } else if(soma < 20){
+      rs2 = "menor";
+   } else {
+      rs2 = "igual";
+   }
+   return "sua soma é " + soma + " que é " + rs1 + " que 10, é "+ rs2 +" de 20";
+}
+console.log(recebe(5,7));
